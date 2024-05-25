b76850a44562a2cb7c2e8357e4f30312d96590fe
Sir-Shiba <ericliang617@gmail.com>
2024-05-25 14:43:28 -0700

    Reverted

--- vh.py
+++ vh.py
@@ -5,23 +5,24 @@ app = Flask(__name__, static_folder='static')
 
 def generate_q1(data, continent):
     question_data = data.get(continent, {})
-    image = question_data.get('image_url', '')
-    answer_bank = question_data.get('options', [])
-    person = question_data.get('question', '')
+    person = random.choice(list(question_data.keys()))
+    answer_bank = list(question_data.keys()).remove(person)
+
+    image = question_data[person]['Image']
     return image, answer_bank, person
 
 @app.route('/')
 def index():
-    with open('questions.json') as f:
+    with open('MrsWRLDWide\womendata.json') as f:
         data = json.load(f)
-    
-    # Dictionary to hold questions for each continent
     questions = {}
-    continents = ['North America', 'Africa', 'Asia', 'Europe', 'Oceania', 'South America']
+    continents = ['North America', 'Africa', 'Asia', 'Europe', 'Australia', 'South America']
     for continent in continents:
-        questions[continent] = generate_q1(data, continent)
+        image, answer_bank, person = generate_q1(data, continent)
+        questions[continent] = {'image': image, 'answer_bank': answer_bank, 'person': person}
 
     return render_template('index.html', questions=questions)
+
 
 
 if __name__ == '__main__':
